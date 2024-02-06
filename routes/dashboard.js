32c7faf1c120ebaf69bd67aa641191e6da09caf5
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-02-06 09:24:41 +0545

    Added route for login/register

--- routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.route("/").get((req, res) => {
-  res.send("Hello there peeps!");
-});
-
-module.exports = router;
---
+++ routes/userRoute.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const {registerUser, loginUser} = require('../controllers/userController')
+
+router.post("/register", registerUser);
+router.post("/", loginUser);
+
+module.exports = router;
